f98ecbcebc3735549136e9649290024ec3947740
Charlotte Chen <charlottechen21@gmail.com>
2016-08-31 16:55:25 -0400

    Get per user commit

--- jobs/github_stats.rb
+++ jobs/github_stats.rb
@@ -20,12 +20,10 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     puts name
     commit_array = Octokit.commits_since(name,'2016-08-30')
     commit_num = commit_array.length
-    puts "Commit Number: #{commit_num}"
     
     # get number of forks per repo
     fork_array =  Octokit.forks(name)
     fork_num = fork_array.length
-    puts "Commit Number: #{fork_num}"
     
     # get last commit activity
     commit_array_history = Octokit.commits(name)
@@ -33,20 +31,40 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     last_commit_commit = last_commit_array[:commit]
     last_commit_committer = last_commit_commit[:committer]
     last_commit_date = last_commit_committer[:date]
-        puts "Commit Array: #{last_commit_array }"
-        puts "Commit Commit: #{last_commit_commit }"
-        puts "Commit Committer: #{ last_commit_committer }"
-        puts "Commit Date: #{last_commit_date }"
+    
+    # get number of contributors
+    contri_stat = Octokit.contributors_stats(name)
+    contri_num = contri_stat[0][:total]
         
+    # get commit counts for each users
+    contri_hash = Hash.new
+    ind_contributors = Octokit.contributors(name)
+    $len = ind_contributors.length
+    $i = 0
+    while $i < $len do
+        contri_name = ind_contributors[$i][:login]
+        contri_commits = ind_contributors[$i][:contributions]
+#        puts "Participation: #{contri_name} => #{contri_commits}"
+        $i += 1
         
+        contri_hash[$i] = {:contri_name => contri_commits}
+    end
+    
+    puts contri_hash
+        
+    # send contributor commit hashmap
     send_event(name, {
-      commits: commit_num,
-      repo: name,
-      issues: r.open_issues_count - pulls,
-      pulls: pulls,
-      forks: fork_num,
-      watchers: r.subscribers_count,
-      activity: last_commit_date
+        contri_hash: contri_hash
     })
-  end
+       
+    # send github data
+    send_event(name, {
+        commits: commit_num,
+        repo: name,
+        forks: fork_num,
+        watchers: r.subscribers_count,
+        activity: last_commit_date,
+        total_contributors: contri_num
+    })
+    end
 end
