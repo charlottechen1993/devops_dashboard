73fc20080b92634a2bdfd48e1df513f255daf911
robert-marriott <robertmarriot@C02S222CFVH8.local>
2016-09-01 16:03:30 -0400

    more jenkins

--- jobs/jenkins_api.rb
+++ jobs/jenkins_api.rb
@@ -5,17 +5,21 @@ require 'jenkins_api_client'
 # The following call will return all jobs matching 'Testjob'
 puts @client.job.list("^sample")
 # The following call lists all jobs
-puts "--------- ROBERT'S JENKINS API DEBUGGING PLAYGROUND --------"
+puts "---=========== ROBERT'S JENKINS API DEBUGGING PLAYGROUND ===========--"
 puts "List of all jobs in targeted jenkins account"
 puts @client.job.list_all
+puts "List of all jobs in targeted jenkins account - with all details (JSON)"
+listallhash = @client.job.list_all_with_details
+puts listallhash
+puts "Test of color to status. This should be blue"
+# puts 
 
 
 
 
 
 
-
-puts "-------- /ROBERT'S JENKINS API DEBUGGING PLAYGROUND --------"
+puts "---=========== /ROBERT'S JENKINS API DEBUGGING PLAYGROUND ===========--"
 
 
 
